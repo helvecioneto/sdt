db748a78ad03fa7e8d959998d607da8dbd07adb3
Helvecio Leal <helvecio.neto@inpe.br>
2020-07-04 17:13:11 -0300

    Select headers

--- sdt/file_action.py
+++ sdt/file_action.py
@@ -15,46 +15,104 @@ def get_files(path_,year):
 
 def open_files(files,path_,year):
 
+    ##Headers from path and year
     headers = extract_header(path_,year)
     
+    ### For file
     for file in sorted(files):
-       
-        try:
-            df = pd.read_table(file,header=None, sep=',')
-        except:
-            df = pd.read_table(file,header=None, sep='\n')
-            df = df[0].str.split(',', expand=True)
+        
+        ### check if file is correct, ignore .git, .DS
+        if file.name.startswith('.') == False:
+            try:
+                df = pd.read_table(file,header=None, sep=',')
+            except:
+                df = pd.read_table(file,header=None, sep='\n')
+                df = df[0].str.split(',', expand=True)
+            ## ID from stations
+            ids = df[0].unique()
 
-        ids = df[0].unique()
-#        print(ids)
-        #Separate ID
-        ddf = {}
-        for ids in ids:
-            ddf[ids] = pd.DataFrame()
-            ddf[ids] = (df.loc[df[0] == ids])
-            ddf[ids] = ddf[ids].dropna(axis=1, how='all')
-            
-            
-            
-            os.system('cls' if os.name == 'nt' else 'clear')
-            for h in range(len(headers)):
-                print(headers[h]['name'])
-                for k in range(len(headers[h]['variables'])):
-                    print(headers[h]['variables'][k][0])
-#                    try:
-#                        if int(headers[h]['variables'][k][0]) == ddf[ids][0].iloc[0]:
-##                    if headers[h]['variables'][k][0] == ddf[ids][0]:
-#                            print(headers[h]['variables'][k])
-#                    except KeyError:
-#                        pass
-#            
-            
-#            print('Processing... File Name:',file)
-            
-#            locat = ddf[ids][0].iloc[0]
-            
-#            print(locat)
-            print(ddf[ids].head(10))
-            input("Press Enter to continue...")
-            os.system('cls' if os.name == 'nt' else 'clear')
-    
+            #Separate ID
+            ddf = {}
+            print(file)
+            for ids in ids:
+                ddf[ids] = pd.DataFrame()
+                ddf[ids] = (df.loc[df[0] == ids])
+                ### Remove Nan from columns
+                ddf[ids] = ddf[ids].dropna(axis=1, how='all')
+                possib_head = []
+                possib_head2 = []
+                names = []
+                os.system('cls' if os.name == 'nt' else 'clear')
+                
+                for h in range(len(headers)):
+                    names.append(headers[h]['name']+headers[h]['datetime'])
+                    for k in range(len(headers[h]['variables'])):
+                        try:
+                            #Lock by ID
+                            if int(headers[h]['variables'][k][0]) == int(ddf[ids][0].iloc[0]):
+                                possib_head2.append(headers[h]['variables'][k])
+                                #Lock by number of columns
+                                if len(headers[h]['variables'][k]) == len(ddf[ids].columns):
+                                    possib_head.append(headers[h]['variables'][k])
+                        except:
+                            pass
+                
+                ### Check if size are diferent
+                if len(possib_head) == 0:
+                    print('Station: ', file.parent.parent.name ,'\t\tYear: ', file.parent.name, '\t\tFile: ', file.name )
+                    print('\n')
+                    print(pd.DataFrame(names).to_string(index=True, header=False))
+
+                    print('Attention, or number of columns are different!')
+                    print('')
+                    phead = pd.DataFrame(possib_head2)
+                    hedd = phead[phead.duplicated(keep='last')]
+                    
+                    ## Check if duplicated
+                    if len(hedd) == 1:
+                        print(hedd.to_string(index=False, header=False))
+                        print(ddf[ids].head(10).iloc[:, : 20].to_string(index=False, header=False))
+                        input("Press Enter to continue...")
+                    else:
+                        print(hedd.to_string(index=True, header=False))
+                        print(ddf[ids].head(10).iloc[:, : 20].to_string(index=False, header=False))
+                        input("Select header Enter to continue...")
+
+                ## Check if duplicated values    
+                if len(possib_head) == 2:
+                    print('Station: ', file.parent.parent.name ,'\t\tYear: ', file.parent.name, '\t\tFile: ', file.name )
+                    print('\n')
+                    print('FSL Names: ')
+                    print('')
+                    print(pd.DataFrame(names).to_string(index=True, header=False))
+                    print('')
+                    phead = pd.DataFrame(possib_head)
+                    hedd = phead[phead.duplicated(keep='last')]
+                    
+                    if len(hedd) == 1:
+                        print(hedd.to_string(index=False, header=False))
+                        print(ddf[ids].head(10).iloc[:, : 20].to_string(index=False, header=False))
+                        input("Press Enter to continue...")
+                    else:
+                        print('\n')
+                        print(pd.DataFrame(possib_head).to_string(index=True, header=True))
+                        print('')
+                        print('\n')
+                        print('Data Frame:')
+                        print(ddf[ids].head(10).iloc[:, : 20].to_string(index=False, header=True))
+                        input("Select header Enter to continue...")
+                    
+                ## Are equal    
+                if len(possib_head) == 1:
+                    print('Station: ', file.parent.parent.name ,'\t\tYear: ', file.parent.name, '\t\tFile: ', file.name )
+                    print('\n')
+                    print('FSL Names: ')
+                    print(pd.DataFrame(names).to_string(index=False, header=False))
+                    print('')
+                    print('\n')
+                    print(pd.DataFrame(possib_head).iloc[:, : 20].to_string(index=False, header=False))
+                    print(ddf[ids].head(10).iloc[:, : 20].to_string(index=False, header=False))
+                    print('')
+                    print('\n')
+                    input("Press Enter to continue...")
+        
