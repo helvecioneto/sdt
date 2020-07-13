ec7160f05f9025f9a8b07857edb428ee9ec99a08
Helvecio Leal <helvecio.neto@inpe.br>
2020-07-13 13:14:15 -0300

    restruct

--- sdt/stations/brb.py
+++ sdt/stations/brb.py
@@ -5,53 +5,59 @@ import numpy as np
 
 def translate_brb(df):
 
-    ### Meteorological data separete
+    # Meteorological data separete
     id_ = str(int(df.iloc[0][0]))
-    meteo = df[[id_,'Year','Day','Min','Temp_sfc', 'Humid', 'Press','Prec','Ws10_AVG','Wd10_AVG','Wd10_STD']]
-    meteo['timestamp'] = meteo['Year'].astype(str) +'/' + meteo['Day'].astype(str) + ' ' + pd.to_timedelta(meteo['Min'],unit='m').astype(str).str[-18:-10]
-    meteo['timestamp'] = pd.to_datetime(meteo['timestamp'],format='%Y/%j %H:%M:%S')
+    meteo = df[[id_, 'Year', 'Day', 'Min', 'Temp_sfc', 'Humid',
+                'Press', 'Prec', 'Ws10_AVG', 'Wd10_AVG', 'Wd10_STD']]
+    meteo['timestamp'] = meteo['Year'].astype(str) + '/' + meteo['Day'].astype(
+        str) + ' ' + pd.to_timedelta(meteo['Min'], unit='m').astype(str).str[-18:-10]
+    meteo['timestamp'] = pd.to_datetime(
+        meteo['timestamp'], format='%Y/%j %H:%M:%S')
     meteo = meteo.rename(columns=str.lower)
     meteo = meteo.set_index('timestamp')
-    
+
     meteo = meteo.rename(columns={id_: 'id'})
-    
-    ## Copy values to create ws10_std
+
+    # Copy values to create ws10_std
     meteo['ws10_std'] = np.nan
-    meteo['ws10_std'] = np.where(meteo.ws10_avg == 3333.0, meteo.ws10_avg, meteo.ws10_std)
-    meteo['ws10_std'] = np.where(meteo.ws10_avg == -5555, meteo.ws10_avg, meteo.ws10_std)
+    meteo['ws10_std'] = np.where(
+        meteo.ws10_avg == 3333.0, meteo.ws10_avg, meteo.ws10_std)
+    meteo['ws10_std'] = np.where(
+        meteo.ws10_avg == -5555, meteo.ws10_avg, meteo.ws10_std)
 
-    ## Conversion types
-    conversion = {'id':'first','year':'first','day':'first','min':'first',
-                  'temp_sfc' : 'first', 'humid' : 'first', 'press' : 'first',
-                  'prec' : 'sum', 'ws10_avg' : 'mean', 'wd10_avg' : 'median', 'wd10_std': 'std', 'ws10_std':'first' }
-    
-    ## Mask to not resample this values
-    Mask = meteo[(meteo != 3333.0) & (meteo != -5555) & (meteo != np.nan)]
-    ## Calculate standard deviation based resample
-    Mask['ws10_std'] = Mask.ws10_avg.resample("10min", how=np.std)
-    
-    ## Apply ressample based conversion
-    Mask = Mask.resample('10min',how=conversion)
+    # Conversion types
+    conversion = {'id': 'first', 'year': 'first', 'day': 'first', 'min': 'first',
+                  'temp_sfc': 'first', 'humid': 'first', 'press': 'first',
+                  'prec': 'sum', 'ws10_avg': 'mean', 'wd10_avg': 'median', 'wd10_std': 'std', 'ws10_std': 'first'}
 
-    ## Final DataFrame
-    meteorological = Mask.fillna(meteo)
+    # Mask to not resample this values
+    Maska = meteo[(meteo != 3333.0) & (meteo != -5555) & (meteo != np.nan)]
+    # Calculate standard deviation based resample
+    Maska['ws10_std'] = Maska['ws10_avg'].resample("10min").agg(np.std)
+
+    # Apply ressample based conversion
+    Maska = Maska.resample('10min').agg(conversion)
+
+    # Final DataFrame
+    meteorological = Maska.fillna(meteo)
     meteorological = meteorological.reset_index()
-    
-    columns = ['id','timestamp','year','day','min','temp_sfc','humid','press','prec',
-                                     'ws10_avg','wd10_avg','wd10_std','ws10_std']
-    meteorological = meteorological.reindex(columns=columns)
 
+    column = ['id', 'timestamp', 'year', 'day', 'min', 'temp_sfc', 'humid', 'press', 'prec',
+              'ws10_avg', 'wd10_avg', 'wd10_std', 'ws10_std']
+    meteorological = meteorological.reindex(columns=column)
 
-    mux = pd.MultiIndex.from_tuples([('id', '',''),('timestamp', '',''),('year', '',''),('day', '',''),('min', '',''),
-                                     ('temp_sfc', 'C','tmo_ventil'), ('humid', '%','tmo_ventil'),('press', 'mb','tmo_ventil'),('prec','mm','tmo_ventil'),
-                                     ('ws10_avg', 'm/s','tmo_ventil'),('wd10_avg', 'm/s','tmo_ventil'),('wd10_std', 'deg','tmo_ventil'),('ws10_std', 'm/s','tmo_ventil')])
-    
-    ### Add multiindex columns
+    mux = pd.MultiIndex.from_tuples([('id', '', ''), ('timestamp', '', ''), ('year', '', ''), ('day', '', ''), ('min', '', ''),
+                                     ('temp_sfc', 'C', 'tmo_ventil'), ('humid', '%', 'tmo_ventil'), (
+                                         'press', 'mb', 'tmo_ventil'), ('prec', 'mm', 'tmo_ventil'),
+                                     ('ws10_avg', 'm/s', 'tmo_ventil'), ('wd10_avg', 'm/s', 'tmo_ventil'), ('wd10_std', 'deg', 'tmo_ventil'), ('ws10_std', 'm/s', 'tmo_ventil')])
+
+    # Add multiindex columns
     meteorological.columns = mux
-    
-    ## Copy dataframe to drop columns
-    solar = df.copy()
-    
-    solar.drop(['Temp_sfc', 'Humid', 'Press','Prec','Ws10_AVG','Wd10_AVG','Wd10_STD','CosAngZen'], axis=1, inplace=True)
-    print(solar)
 
+    print(meteorological)
+    # Copy dataframe to drop columns
+    solar = df.copy()
+
+    solar.drop(['Temp_sfc', 'Humid', 'Press', 'Prec', 'Ws10_AVG',
+                'Wd10_AVG', 'Wd10_STD', 'CosAngZen'], axis=1, inplace=True)
+    # print(solar)
---
+++ tests/test.py
@@ -0,0 +1,8 @@
+import unittest
+from src.main import printer
+
+
+class printer(unittest.TestCase):
+    def test_printer(self):
+        print('teste  uni')
+        return None
